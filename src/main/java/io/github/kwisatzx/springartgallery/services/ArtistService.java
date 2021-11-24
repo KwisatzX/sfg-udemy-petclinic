5cc6a573081084b34ac69978178b3fcaca680bf9
KwisatzX <kwisatz4@gmail.com>
2021-11-24 16:12:54 +0100

    Refactored services.

--- src/main/java/io/github/kwisatzx/springartgallery/services/ArtistService.java
+++ src/main/java/io/github/kwisatzx/springartgallery/services/ArtistService.java
@@ -2,16 +2,9 @@ package io.github.kwisatzx.springartgallery.services;
 
 import io.github.kwisatzx.springartgallery.model.Artist;
 
-import java.util.Set;
+public interface ArtistService extends CrudService<Artist, Long> {
 
-public interface ArtistService {
     Artist findByLastName(String lastName);
 
     Artist findByPseudonym(String pseudonym);
-
-    Artist findById(Long id);
-
-    Artist save(Artist artist);
-
-    Set<Artist> findAll();
 }
--- src/main/java/io/github/kwisatzx/springartgallery/services/PaintingService.java
+++ src/main/java/io/github/kwisatzx/springartgallery/services/PaintingService.java
@@ -2,16 +2,9 @@ package io.github.kwisatzx.springartgallery.services;
 
 import io.github.kwisatzx.springartgallery.model.Painting;
 
-import java.util.Set;
+public interface PaintingService extends CrudService<Painting, Long> {
 
-public interface PaintingService {
     Painting findByName(String name);
 
     Painting findByArtist(String artistName);
-
-    Painting findById(Long id);
-
-    Painting save(Painting painting);
-
-    Set<Painting> findAll();
 }
